8df64bbea6bf6eb0a1b669f88314407f110ac4ee
Khaled Hossain <khaledhossain778@gmail.com>
2024-10-22 22:22:38 +0600

    project setup

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,12 +6,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
-
+    use HasApiTokens, HasFactory, Notifiable;
+    
     /**
      * The attributes that are mass assignable.
      *
@@ -21,8 +21,9 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role',
     ];
-
+    
     /**
      * The attributes that should be hidden for serialization.
      *
@@ -32,17 +33,28 @@ class User extends Authenticatable
         'password',
         'remember_token',
     ];
-
+    
     /**
-     * Get the attributes that should be cast.
+     * The attributes that should be cast.
      *
-     * @return array<string, string>
+     * @var array<string, string>
      */
-    protected function casts(): array
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+    
+    public function isAdmin(): bool
     {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
+        return $this->role === 'admin';
+    }
+    
+    public function isTutor(): bool
+    {
+        return $this->role === 'tutor';
+    }
+    
+    public function isParent(): bool
+    {
+        return $this->role === 'parent';
     }
 }
