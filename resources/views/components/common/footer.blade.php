b8ff96ff7064d8a6bd056a8802091341fc375500
Khaled Hossain <khaledhossain778@gmail.com>
2024-10-25 13:08:14 +0600

    header-footer design

--- resources/views/components/common/footer.blade.php
+++ resources/views/components/common/footer.blade.php
@@ -1,22 +1,28 @@
-<footer class="footer">
-	<div class="container-fluid d-flex justify-content-between">
-		<nav class="pull-left">
-			<ul class="nav">
+<footer class="footer-center bg-gray-800">
+	<div class="navbar">
+		<nav class="navbar-start">
+			<ul class="flex gap-2">
 				<li class="nav-item">
-					<a class="nav-link" href="#">Help</a>
+					<a class="nav-link text-white hover:text-orange-700" href="#">FAQs</a>
 				</li>
 				<li class="nav-item">
-					<a class="nav-link" href="#">Contact</a>
+					<a class="nav-link text-white hover:text-orange-700" href="#">About Us</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link text-white hover:text-orange-700" href="#">Contact</a>
 				</li>
 			</ul>
 		</nav>
 		
-		<div class="copyright">
-			&copy 2024 | Made with <i class="fa fa-heart heart text-danger"></i> by
-			<a href="#">Khaled Hossain</a>
+		<div class="navbar-center">
+			<p class="text-white">
+				Developed by
+				<a target="_blank" class="hover:text-orange-700" href="#"> khaled.dev</a>
+			</p>
 		</div>
-		<div>
-			Distributed by <a target="_blank" href="#">ThemeWagon</a>.
+		
+		<div class="navbar-end">
+			<p class="text-white">&copy 2024</p>
 		</div>
 	</div>
 </footer>
