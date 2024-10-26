3795eff27145d234bdc959eb2e40768e7288c4fb
Khaled Hossain <khaledhossain778@gmail.com>
2024-10-26 10:23:09 +0600

    footer updated

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -15,13 +15,24 @@
 	{{-- Navbar --}}
 	@include('components.common.header')
 	
-	<div class="flex p-80">
-		<div class="mx-auto">
-			<button class="btn btn-primary btn-sm btn-white waves-effect bg-gradient">
-				Click Me <i class="fas fa-video"></i>
-			</button>
+	
+	<div class="hero bg-base-200 min-h-screen">
+		<div class="hero-content flex-col lg:flex-row-reverse">
+			<div>
+				<img src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp" class="max-w-sm rounded-lg shadow-2xl" alt="Hero Image"/>
+			</div>
+			<div>
+				<h1 class="text-5xl font-bold">Box Office News!</h1>
+				<p class="py-6">
+					Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+					quasi. In deleniti eaque aut repudiandae et a id nisi.
+				</p>
+				<button class="btn btn-primary btn-sm btn-white waves-effect bg-gradient">
+					Get Started&nbsp;<i class="fas fa-play"></i></button>
+			</div>
 		</div>
 	</div>
+	
 	
 	@include('components.common.footer')
 </div>
