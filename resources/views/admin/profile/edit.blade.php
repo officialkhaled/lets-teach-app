ff2773b2deceb8116349fd2cd37f546aa8d15dff
Khaled <khaled@skylarksoft.com>
2024-12-04 11:55:54 +0600

    code refactor

--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -7,7 +7,7 @@
 				<div class="col-md-12">
 					<section class="block block-rounded block-bordered block-link-shadow" style="padding: 30px;">
 						<header class="d-flex justify-content-between">
-							<div class="">
+							<div>
 								<h3 class="text-lg font-medium text-gray-900">
 									Profile Information
 								</h3>
@@ -15,16 +15,12 @@
 									Update your account's profile information and email address.
 								</p>
 							</div>
-							<div class="">
+							<div>
 								<a href="{{ route('admin.admin-dashboard') }}" class="btn btn-info btn-sm waves-effect bg-gradient">
 									&nbsp;<i class="fa-regular fa-circle-left opacity-50"></i>&nbsp;&nbsp;Back&nbsp;
 								</a>
 							</div>
 						</header>
-						
-						<form id="send-verification" method="post" action="{{ route('verification.send') }}">
-							@csrf
-						</form>
 						
 						<div class="row">
 							<form method="post" action="{{ route('admin.profile.update') }}" class="space-y-3" enctype="multipart/form-data">
@@ -69,37 +65,15 @@
 								</div>
 							</form>
 						</div>
-					
 					</section>
 				</div>
-				
-				
-				{{--				<div class="py-12">--}}
-				{{--					<div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">--}}
-				{{--						<div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">--}}
-				{{--							<div class="max-w-xl">--}}
-				{{--								@include('profile.partials.update-profile-information-form')--}}
-				{{--							</div>--}}
-				{{--						</div>--}}
-				{{--						--}}
-				{{--						<div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">--}}
-				{{--							<div class="max-w-xl">--}}
-				{{--								@include('profile.partials.update-password-form')--}}
-				{{--							</div>--}}
-				{{--						</div>--}}
-				{{--						--}}
-				{{--						<div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">--}}
-				{{--							<div class="max-w-xl">--}}
-				{{--								@include('profile.partials.delete-user-form')--}}
-				{{--							</div>--}}
-				{{--						</div>--}}
-				{{--					</div>--}}
-				{{--				</div>--}}
-			
 			</div>
 		</div>
 	</main>
-	
+
+@endsection
+
+@section('script')
 	<script>
         function previewImage(event) {
             const input = event.target;
@@ -116,5 +90,4 @@
             }
         }
 	</script>
-
 @endsection
