e8773b990968787a7bb18f8272dadd4fee2b0f54
Khaled Hossain <khaledhossain778@gmail.com>
2024-12-31 22:24:42 +0600

    tutor dashboard design update

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -7,24 +7,6 @@ use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Tag extends Model
 {
-    public const CLASS = 1;
-    public const SUBJECT = 2;
-    
-    public const TYPE = [
-        1 => 'Class',
-        2 => 'Subject',
-    ];
-    
-    public const CLASSES = [
-        1 => 'Class 1',
-        2 => 'Class 2',
-    ];
-    
-    public const SUBJECTS = [
-        1 => 'Physics',
-        2 => 'Chemistry',
-    ];
-    
     protected $table = 'tags';
     
     protected $fillable = [
