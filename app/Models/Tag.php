d6ec6db4f98786238831beb0f998b3d45de92391
Khaled <khaled@skylarksoft.com>
2024-12-31 10:32:42 +0600

    update: Tags Management in-progress

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -4,10 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasOne;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Tag extends Model
 {
+    public const CLASS = 1;
+    public const SUBJECT = 2;
+    
+    public const TYPE = [
+        1 => 'Class',
+        2 => 'Subject',
+    ];
+    
+    public const CLASSES = [
+        1 => 'Class 1',
+        2 => 'Class 2',
+    ];
+    
+    public const SUBJECTS = [
+        1 => 'Physics',
+        2 => 'Chemistry',
+    ];
+    
     protected $table = 'tags';
     
     protected $fillable = [
