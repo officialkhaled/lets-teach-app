4cf216c5ba4696b63f4dc3819218cf493b87e5d8
Khaled Hossain <khaledhossain778@gmail.com>
2025-02-05 21:40:28 +0600

    refactor

--- app/Http/Controllers/TagManagementController.php
+++ app/Http/Controllers/TagManagementController.php
@@ -9,45 +9,47 @@ class TagManagementController extends Controller
 {
     public function index()
     {
-        $tags = Tag::query()->latest()->get();
-        
+        $tags = Tag::query()
+            ->latest()
+            ->get();
+
         return view('admin.tags-management.list', [
             'tags' => $tags,
         ]);
     }
-    
+
     public function create()
     {
         return view('admin.tags-management.create');
     }
-    
+
     public function store(Request $request)
     {
         Tag::create([
             'name' => $request->input('name'),
             'type' => $request->input('type'),
         ]);
-        
+
         return redirect()->route('admin.tags-management.index')->with('success', 'Tag Added Successfully!');
     }
-    
+
     public function edit(Tag $tag)
     {
         return view('admin.tags-management.edit-tag', [
             'tag' => $tag,
         ]);
     }
-    
+
     public function update(Request $request, Tag $tag)
     {
         $tag->update([
             'name' => $request->input('name'),
             'type' => $request->input('type'),
         ]);
-        
+
         return redirect()->route('admin.tags-management.index')->with('success', 'Tag Updated Successfully!');
     }
-    
+
     public function destroy(Tag $tag)
     {
         $tag->delete();
