b906ebc391eea53b6ac8edb3a172f6016f73b85d
Khaled <khaled@skylarksoft.com>
2025-03-06 09:16:43 +0600

    Json.php fix

--- app/Casts/Json.php
+++ app/Casts/Json.php
@@ -6,12 +6,12 @@ use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
 
 class Json implements CastsAttributes
 {
-    public function get($model, $key, $value, $attributes): array
+    public function get($model, $key, $value, $attributes)
     {
         return json_decode($value, true);
     }
 
-    public function set($model, $key, $value, $attributes): ?string
+    public function set($model, $key, $value, $attributes)
     {
         return $value ? json_encode($value) : null;
     }
