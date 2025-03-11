d431ceb1c48e09794db3002e81853c34f1b7f9ac
Khaled <khaled@skylarksoft.com>
2025-03-11 10:55:36 +0600

    auth UI update

--- resources/views/auth/confirm-password.blade.php
+++ resources/views/auth/confirm-password.blade.php
@@ -1,27 +1,77 @@
-<x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600">
-        {{ __('This is a secure area of the application. Please confirm your password before continuing.') }}
-    </div>
+@extends('auth-layout')
+@section('title', 'Confirm Password')
+@section('content')
 
-    <form method="POST" action="{{ route('password.confirm') }}">
-        @csrf
+    <main id="main-container">
+        <div class="bg-body-dark">
+            <div class="hero-static content content-full px-1">
+                <div class="row mx-0 justify-content-center">
+                    <div class="col-lg-8 col-xl-6">
+                        <div class="py-4 text-center">
+                            <a class="link-fx fw-bold" href="#">
+                                <i class="fa fa-fire"></i>
+                                <span class="fs-4 text-body-color">Let's</span><span class="fs-4">Teach</span>
+                            </a>
+                            <h2 class="h3 fw-bold mt-4 mb-1">
+                                Confirm your password
+                            </h2>
+                            <h5 class="fs-5 lh-base fw-normal text-muted mb-0">
+                                This is a secure area of the application. Please confirm your password before continuing.
+                            </h5>
+                        </div>
 
-        <!-- Password -->
-        <div>
-            <x-input-label for="password" :value="__('Password')"/>
+                        <form action="{{ route('password.confirm') }}" method="POST">
+                            @csrf
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                          type="password"
-                          name="password"
-                          required autocomplete="current-password"/>
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2"/>
+                            <div class="block block-themed block-rounded block-fx-shadow">
+                                <div class="block-header bg-gd-dusk">
+                                    <h3 class="block-title">Confirm Password</h3>
+                                </div>
+                                <div class="block-content">
+                                    <div class="mb-4">
+                                        <label class="form-label" for="password">Password</label>
+                                        <input type="password" class="form-control" id="password" name="password"
+                                               required autocomplete="current-password" placeholder="Enter Password">
+                                    </div>
+                                </div>
+                                <div class="block-content block-content-full bg-body-light text-center d-flex justify-content-between">
+                                    <button type="submit" class="btn btn-sm btn-alt-primary fw-medium">
+                                        <i class="fa-solid fa-save opacity-75"></i>&nbsp;&nbsp;Confirm&nbsp;
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
+    </main>
 
-        <div class="flex justify-end mt-4">
-            <x-primary-button>
-                {{ __('Confirm') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+@endsection
+
+
+{{--<x-guest-layout>--}}
+{{--    <div class="mb-4 text-sm text-gray-600">--}}
+{{--        {{ __('This is a secure area of the application. Please confirm your password before continuing.') }}--}}
+{{--    </div>--}}
+
+{{--    <form method="POST" action="{{ route('password.confirm') }}">--}}
+{{--        @csrf--}}
+
+{{--        <!-- Password -->--}}
+{{--        <div>--}}
+{{--            <x-input-label for="password" :value="__('Password')"/>--}}
+
+{{--            <x-text-input id="password" class="block mt-1 w-full"--}}
+{{--                          type="password" name="password" required autocomplete="current-password"/>--}}
+
+{{--            <x-input-error :messages="$errors->get('password')" class="mt-2"/>--}}
+{{--        </div>--}}
+
+{{--        <div class="flex justify-end mt-4">--}}
+{{--            <x-primary-button>--}}
+{{--                {{ __('Confirm') }}--}}
+{{--            </x-primary-button>--}}
+{{--        </div>--}}
+{{--    </form>--}}
+{{--</x-guest-layout>--}}
--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -1,31 +1,92 @@
-<x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600">
-        {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
-    </div>
+@extends('auth-layout')
+@section('title', 'Verify Email')
+@section('content')
 
-    @if (session('status') == 'verification-link-sent')
-        <div class="mb-4 font-medium text-sm text-green-600">
-            {{ __('A new verification link has been sent to the email address you provided during registration.') }}
-        </div>
-    @endif
+    <main id="main-container">
+        <div class="bg-body-dark">
+            <div class="hero-static content content-full px-1">
+                <div class="row mx-0 justify-content-center">
+                    <div class="col-lg-8 col-xl-6">
+                        <div class="py-4 text-center">
+                            <a class="link-fx fw-bold" href="#">
+                                <i class="fa fa-fire"></i>
+                                <span class="fs-4 text-body-color">Let's</span><span class="fs-4">Teach</span>
+                            </a>
+                            <h2 class="h3 fw-bold mt-4 mb-1">
+                                Verify your Email
+                            </h2>
+                            <h5 class="fs-5 lh-base fw-normal text-muted mb-0">
+                                Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn't receive the email, we will gladly send you another.
+                            </h5>
+                        </div>
 
-    <div class="mt-4 flex items-center justify-between">
-        <form method="POST" action="{{ route('verification.send') }}">
-            @csrf
+                        @if (session('status') == 'verification-link-sent')
+                            <div class="mb-4 font-medium text-sm text-green-600">
+                                {{ __('A new verification link has been sent to the email address you provided during registration.') }}
+                            </div>
+                        @endif
 
-            <div>
-                <x-primary-button>
-                    {{ __('Resend Verification Email') }}
-                </x-primary-button>
+                        <form action="{{ route('verification.send') }}" method="POST">
+                            @csrf
+
+                            <div class="block block-themed block-rounded block-fx-shadow">
+                                <div class="block-header bg-gd-dusk">
+                                    <h3 class="block-title">Verify Email</h3>
+                                </div>
+                                <div class="block-content">
+                                    <div class="mb-4">
+                                        <button type="submit" class="btn btn-sm btn-alt-primary fw-medium">
+                                            <i class="fa-solid fa-save opacity-75"></i>&nbsp;&nbsp;Resend Verification Email&nbsp;
+                                        </button>
+                                    </div>
+                                </div>
+                                <div class="block-content block-content-full bg-body-light text-center d-flex justify-content-between">
+                                    <form method="POST" action="{{ route('logout') }}">
+                                        @csrf
+
+                                        <button type="submit" class="btn btn-sm btn-alt-danger fw-medium">
+                                            <i class="fa fa-fw fa-sign-out-alt opacity-75"></i>&nbsp;&nbsp;Sign Out&nbsp;
+                                        </button>
+                                    </form>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
             </div>
-        </form>
+        </div>
+    </main>
 
-        <form method="POST" action="{{ route('logout') }}">
-            @csrf
+@endsection
 
-            <button type="submit" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                {{ __('Log Out') }}
-            </button>
-        </form>
-    </div>
-</x-guest-layout>
+{{--<x-guest-layout>--}}
+{{--    <div class="mb-4 text-sm text-gray-600">--}}
+{{--        {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}--}}
+{{--    </div>--}}
+
+{{--    @if (session('status') == 'verification-link-sent')--}}
+{{--        <div class="mb-4 font-medium text-sm text-green-600">--}}
+{{--            {{ __('A new verification link has been sent to the email address you provided during registration.') }}--}}
+{{--        </div>--}}
+{{--    @endif--}}
+
+{{--    <div class="mt-4 flex items-center justify-between">--}}
+{{--        <form method="POST" action="{{ route('verification.send') }}">--}}
+{{--            @csrf--}}
+
+{{--            <div>--}}
+{{--                <x-primary-button>--}}
+{{--                    {{ __('Resend Verification Email') }}--}}
+{{--                </x-primary-button>--}}
+{{--            </div>--}}
+{{--        </form>--}}
+
+{{--        <form method="POST" action="{{ route('logout') }}">--}}
+{{--            @csrf--}}
+
+{{--            <button type="submit" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">--}}
+{{--                {{ __('Log Out') }}--}}
+{{--            </button>--}}
+{{--        </form>--}}
+{{--    </div>--}}
+{{--</x-guest-layout>--}}
